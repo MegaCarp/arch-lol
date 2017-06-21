aaf8f12918bc6d4cfca987745106c1099564aec1
MegaCarp <stashkodenis@gmail.com>
2017-06-21 04:23:43 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 export WINEARCH=win32 WINEPREFIX=~/games/lol-from-a_wiki
-winecfg
+winecfg     #CHECK IF THIS IS NEEDED. winecfg is needed to trigger installation of mono and gecko. 
 sudo pacman -S --needed --noconfirm lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-libldap lib32-lcms2 lib32-gnutls  
 winetricks d3dx9 vcrun2005 wininet corefonts
 ln -s /media/Archive/games/LeagueOfLegends/ $WINEPREFIX
@@ -8,3 +8,4 @@ echo 'Access the libraries tab, and add :
 msvcp140 (Native, then builtin)
 vcomp140 (Native, then builtin)
 vcruntime140 (Native, then builtin)'
+unset -v WINEARCH WINEPREFIX
