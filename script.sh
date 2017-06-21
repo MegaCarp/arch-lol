b6d2b0354de0c63b878c725b1ea7081b52915856
MegaCarp <stashkodenis@gmail.com>
2017-06-21 04:19:38 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 export WINEARCH=win32 WINEPREFIX=~/games/lol-from-a_wiki
 winecfg
-sudo pacman -S lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-libldap lib32-lcms2 lib32-gnutls
-man pacman
-sudo pacman -S lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-libldap lib32-lcms2 lib32-gnutls
-winetricks d3dx9 vcrun2005 wininet corefonts adobeair ie8
+sudo pacman -S --needed --noconfirm lib32-alsa-lib lib32-alsa-plugins lib32-libpulse lib32-libldap lib32-lcms2 lib32-gnutls  
+winetricks d3dx9 vcrun2005 wininet corefonts
+ln -s /media/Archive/games/LeagueOfLegends/ $WINEPREFIX
 winecfg
-wine '/home/personal/games/lol-from-a_wiki/League of Legends/LeagueClient.exe'
+echo 'Access the libraries tab, and add :
+msvcp140 (Native, then builtin)
+vcomp140 (Native, then builtin)
+vcruntime140 (Native, then builtin)'
