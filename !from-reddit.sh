970ae41b28b15be1a5706f717e915ffd8477d1d2
MegaCarp <stashkodenis@gmail.com>
2017-08-01 11:00:37 +0300

    Update !from-reddit.sh

--- !from-reddit.sh
+++ !from-reddit.sh
@@ -2,8 +2,8 @@
 sudo pacman -S --needed --noconfirm lib32-libldap alsa-lib gnutls lcms2 lib32-alsa-lib lib32-gnutls lib32-lcms2 lib32-libldap lib32-mpg123 libldap mpg123 gendesk icoutils imagemagick alsa-plugins lib32-alsa-plugins lib32-libpulse libpulse openal lib32-openal
 
 export WINEARCH=win32 WINEPREFIX=~/games/lol-from_reddit
-echo 'wiki.archlinux.org/index.php/League_of_Legends#Wine_Method' > $WINEPREFIX/source_instructions.txt
 winetricks d3dx9
+echo 'wiki.archlinux.org/index.php/League_of_Legends#Wine_Method' > $WINEPREFIX/source_instructions.txt
 ln -s /media/Archive/games/LeagueOfLegends/ $WINEPREFIX
 winecfg
 #override msvcp140
