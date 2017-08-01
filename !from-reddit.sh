fc3121d322d0460b698036a97703da1c30c699d6
MegaCarp <stashkodenis@gmail.com>
2017-08-01 11:26:40 +0300

    Update !from-reddit.sh

--- !from-reddit.sh
+++ !from-reddit.sh
@@ -1,17 +1,15 @@
 #depts for wine and lol. lib32-libldap is arch specific (check?)
-sudo pacman -S --needed --noconfirm lib32-libldap alsa-lib gnutls lcms2 lib32-alsa-lib lib32-gnutls lib32-lcms2 lib32-libldap lib32-mpg123 libldap mpg123 gendesk icoutils imagemagick alsa-plugins lib32-alsa-plugins lib32-libpulse libpulse openal lib32-openal
+sudo apt-get install lib32-libldap alsa-lib gnutls lcms2 lib32-alsa-lib lib32-gnutls lib32-lcms2 lib32-libldap lib32-mpg123 libldap mpg123 gendesk icoutils imagemagick alsa-plugins lib32-alsa-plugins lib32-libpulse libpulse openal lib32-openal -y
 
 export WINEARCH=win32 WINEPREFIX=~/games/lol-from_reddit
 winetricks d3dx9
 echo 'wiki.archlinux.org/index.php/League_of_Legends#Wine_Method' > $WINEPREFIX/source_instructions.txt
-ln -s /media/Archive/games/LeagueOfLegends/ $WINEPREFIX
+ln -s ~/games/backup_games/League\ of\ Legends $WINEPREFIX
 winecfg
 #override msvcp140
 #should also enable CSMT but not until you have a proper video card
-echo 'make msvcp140 (Native, then builtin)
 
-wine $WINEPREFIX/LeagueOfLegends/LeagueClient.exe
-
+wine $WINEPREFIX/League\ of\ Legends/LeagueClient.exe
 unset -v WINEARCH WINEPREFIX
 
 sudo bash -c "cat <<EOF > /usr/share/applications/lol.desktop
@@ -20,9 +18,9 @@ Encoding=UTF-8
 Version=1.0                                     # version of an app.
 Name=League of Legends                                 # name of an app.
 Comment=Moba from Riot                # comment which appears as a tooltip.
-Exec=WINEARCH=win32 WINEPREFIX=/home/personal/games/lol_from-reddit/ wine /home/personal/games/lol_from-reddit/LeagueOfLegends/LeagueClient.exe  # command used to launch an app.
+Exec=WINEARCH=win32 WINEPREFIX=~/games/lol-from_reddit wine $WINEPREFIX/League\ of\ Legends/LeagueClient.exe
 Terminal=false                                  # whether an app requires to be run in a terminal.
-Icon=/home/personal/games/LeagueOfLegends/icon.png   # location of icon file.
+Icon=~/games/backup_games/League\ of\ Legends/icon.png   # location of icon file.
 Type=Application                                # type.
 Categories=Games;Wine;        # categories in which this app should be listed.
 EOF"
